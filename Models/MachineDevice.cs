9e54cc0f37f574d6e4131eaddac59f786b15f1c0
Zahed Davodi <zaheddavodibaloch@gmail.com>
2022-10-04 17:00:34 +0200

    Add project files.

---
+++ Models/MachineDevice.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagementSystem.Models
+{
+    public class MachineDevice
+    {
+        public string DeviceId { get; set; } = Guid.NewGuid().ToString("n");
+        public string DeviceName { get; set; } = string.Empty;
+        public bool Status { get; set; }
+        public List<MachineDevice> Data { get; set; } = new List<MachineDevice>();
+    }
+}
